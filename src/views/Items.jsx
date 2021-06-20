8ebebdce5d078b3ccb06803782027a8e8a3d8953
Ashik Rahman <ashrhmn99@gmail.com>
2021-06-21 05:33:59 +0600

    Added delete to Items

--- src/views/Items.jsx
+++ src/views/Items.jsx
@@ -32,17 +32,32 @@ const Td = ({ data }) => {
 };
 
 const Tr = ({ item }) => {
-  const [savingTxt, setSavingTxt] = useState("");
   const queryClient = useQueryClient();
-  const { mutate } = useMutation((item) => ItemsApi.updateItem(item), {
-    onSuccess: () => {
-      queryClient.invalidateQueries(["items"]);
-      setEditing(false);
-      setId("");
-      setName("");
-      setPrice("");
-    },
-  });
+  const { mutate: updateData } = useMutation(
+    (item) => ItemsApi.updateItem(item),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["items"]);
+        setEditing(false);
+        setId("");
+        setName("");
+        setPrice("");
+      },
+    }
+  );
+
+  const { mutate: deleteData } = useMutation(
+    (item) => ItemsApi.deleteItem(item.id),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["items"]);
+        setEditing(false);
+        setId("");
+        setName("");
+        setPrice("");
+      },
+    }
+  );
 
   const [editing, setEditing] = useState(false);
 
@@ -51,19 +66,25 @@ const Tr = ({ item }) => {
   const [price, setPrice] = useState(0.0);
   const handleEdit = () => {
     if (editing) {
-      setSavingTxt("Saving...");
-      mutate({
+      updateData({
         id: id,
         name: name,
         price: price,
       });
     } else {
-      setSavingTxt("");
       setEditing(true);
       setId(item.id);
       setName(item.name);
       setPrice(item.price);
     }
+  };
+
+  const handleDelete = () => {
+    deleteData({
+      id: id,
+      name: name,
+      price: price,
+    });
   };
   return (
     <>
@@ -73,7 +94,9 @@ const Tr = ({ item }) => {
         <Td data={item.price} />
         <td className="px-12">
           <button
-            className={`bg-${editing?'green':'blue'}-500 text-white rounded-md p-1`}
+            className={`bg-${
+              editing ? "green" : "blue"
+            }-500 text-white rounded-md p-1`}
             onClick={handleEdit}
           >
             {editing ? "Save" : "Edit"}
@@ -88,7 +111,7 @@ const Tr = ({ item }) => {
           id={id}
           name={name}
           price={price}
-          savingTxt={savingTxt}
+          handleDelete={handleDelete}
         />
       ) : (
         <></>
@@ -97,7 +120,15 @@ const Tr = ({ item }) => {
   );
 };
 
-const EditForm = ({ id, name, price, setId, setName, setPrice, savingTxt }) => {
+const EditForm = ({
+  id,
+  name,
+  price,
+  setId,
+  setName,
+  setPrice,
+  handleDelete,
+}) => {
   return (
     <tr className="bg-green-500">
       <td className="p-3 px-8">
@@ -118,13 +149,20 @@ const EditForm = ({ id, name, price, setId, setName, setPrice, savingTxt }) => {
       </td>
       <td className="p-3 px-8">
         <input
-          className="w-40"
+          className="w-40 text-right"
           type="text"
           value={price}
           onChange={(e) => setPrice(e.target.value)}
         />
       </td>
-      <td className="text-white text-center">{savingTxt}</td>
+      <td className="px-12">
+        <button
+          className={`bg-red-500 text-white rounded-md p-1`}
+          onClick={handleDelete}
+        >
+          Delete
+        </button>
+      </td>
     </tr>
   );
 };
